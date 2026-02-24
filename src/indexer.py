c4c5d9ce34e302c60c1528cb572a060b81383bab
aonacollegedemo <evabansal14@gmail.com>
2026-02-24 10:41:36 +0530

    feat: implement preprocessing in add_document

--- src/indexer.py
+++ src/indexer.py
@@ -3,3 +3,9 @@ class Indexer:
                         self.inverted_index = {}
         self.total_docs = 0
         self.doc_lengths = {}
+
+        def add_document(self, doc_id, text):
+                        self.total_docs += 1
+                        text = text.lower()
+                        words = text.split()
+                        self.doc_lengths[doc_id] = len(words)
